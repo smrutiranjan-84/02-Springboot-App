90c38a3b4b90d5c1bda977c8c2ab7181f23d788d
unknown <Dev2@gmail.com>
2021-10-05 07:35:47 +0530

    Car class modified

---
+++ src/main/java/com/example/demo/beans/Car.java
@@ -0,0 +1,15 @@
+package com.example.demo.beans;
+
+public class Car {
+	
+		Car(){
+			System.out.println("Car ::constructor");
+		}
+		public void drive() {
+			System.out.println("car is driving");
+		}
+
+	}
+
+
+
